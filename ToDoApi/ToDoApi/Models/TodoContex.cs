d7c06a5a6fb334384b14d19fa619a688ce34be53
Nikita <nike04@mail.ru>
2020-11-27 00:35:20 +0500

    add ToDoApi

---
+++ ToDoApi/ToDoApi/Models/TodoContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<ToDoItem> TodoItems { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+}
